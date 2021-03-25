1f46ad699a01add528930218fef95b817ffbf758
felix <felix@nexumnet.com>
2021-03-24 20:04:21 -0400

    code optimization, new CreateCommand() function to help code structure

--- kinova3.py
+++ kinova3.py
@@ -4,8 +4,7 @@ import time
 import threading
 import usb.core
 import usb.util
-
-from time import gmtime, strftime
+import utilities
 from kortex_api.TCPTransport import TCPTransport
 from kortex_api.RouterClient import RouterClient
 from kortex_api.SessionManager import SessionManager
@@ -13,15 +12,6 @@ from kortex_api.autogen.client_stubs.BaseClientRpc import BaseClient
 from kortex_api.autogen.client_stubs.BaseCyclicClientRpc import BaseCyclicClient
 from kortex_api.autogen.messages import Base_pb2, BaseCyclic_pb2, Common_pb2
 TIMEOUT_DURATION = 10
-
-def check_for_end_or_abort(e):
-    def check(notification, e = e):
-        print("EVENT : " + \
-              Base_pb2.ActionEvent.Name(notification.action_event))
-        if notification.action_event == Base_pb2.ACTION_END \
-        or notification.action_event == Base_pb2.ACTION_ABORT:
-            e.set()
-    return check
   
 def main():
     dev = usb.core.find(idVendor=0x256f, idProduct=0xc635)
@@ -34,143 +24,70 @@ def main():
     if dev.is_kernel_driver_active(0):
         reattach = True
         dev.detach_kernel_driver(0)
-
     ep_in = dev[0][(0,0)][0]
     ep_out = dev[0][(0,0)][1]
     
     # Import the utilities helper module
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
-    import utilities
-    # Parse arguments
-    args = utilities.parseConnectionArguments()
-    # Create connection to the device and get the router
-    with utilities.DeviceConnection.createTcpConnection(args) as router:
-        # Create required services
-        base = BaseClient(router)
-        # Example core
-        success = True
+    args = utilities.parseConnectionArguments() # Parse arguments
+    with utilities.DeviceConnection.createTcpConnection(args) as router: # Create connection and get router
+        base = BaseClient(router) # Create required services
         while 1:
             try:
                 data = dev.read(ep_in.bEndpointAddress, ep_in.bLength, 0)
+                def CreateCommand(vel, axys, mode):
+                    command = Base_pb2.TwistCommand()
+                    command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_TOOL
+                    command.duration = 0
+                    twist = command.twist
+                    if mode == "linear":
+                        if axys == "x":twist.linear_x = vel
+                        elif axys == "y":twist.linear_y = vel
+                        elif axys == "z":twist.linear_z = vel
+                    elif mode == "angular":
+                        if axys == "x":twist.angular_x = vel
+                        elif axys == "y":twist.angular_y = vel
+                        elif axys == "z":twist.angular_z = vel
+                    base.SendTwistCommand(command)
+                    base.Stop()
+                    
+                def check_for_end_or_abort(e):
+                    def check(notification, e = e):
+                        print("EVENT : " + \
+                              Base_pb2.ActionEvent.Name(notification.action_event))
+                        if notification.action_event == Base_pb2.ACTION_END \
+                        or notification.action_event == Base_pb2.ACTION_ABORT:
+                            e.set()
+                    return check
+                    
                 if data[0] == 1:
                     tx = data[1] + (data[2]*256)
                     ty = data[3] + (data[4]*256)
                     tz = data[5] + (data[6]*256)
-                    if data[2] > 127:
-                        tx -= 65536
-                    if data[4] > 127:
-                        ty -= 65536
-                    if data[6] > 127:
-                        tz -= 65536
+                    if data[2] > 127:tx -= 65536
+                    if data[4] > 127:ty -= 65536
+                    if data[6] > 127:tz -= 65536
                     print ("T: ", tx, ty, tz)
-                    if tx >= 100:
-                        command = Base_pb2.TwistCommand()
-                        command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_TOOL
-                        command.duration = 0
-                        twist = command.twist
-                        twist.linear_x = 1
-                        base.SendTwistCommand(command)
-                        base.Stop()
-                    if tx <= -100:
-                        command = Base_pb2.TwistCommand()
-                        command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_TOOL
-                        command.duration = 0
-                        twist = command.twist
-                        twist.linear_x = -1
-                        base.SendTwistCommand(command)
-                        base.Stop()
-                    if ty >= 100:
-                        command = Base_pb2.TwistCommand()
-                        command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_TOOL
-                        command.duration = 0
-                        twist = command.twist
-                        twist.linear_y = 1
-                        base.SendTwistCommand(command)
-                        base.Stop()
-                    if ty <= -100:
-                        command = Base_pb2.TwistCommand()
-                        command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_TOOL
-                        command.duration = 0
-                        twist = command.twist
-                        twist.linear_y = -1
-                        base.SendTwistCommand(command)
-                        base.Stop()
-                    if tz >= 100:
-                        command = Base_pb2.TwistCommand()
-                        command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_TOOL
-                        command.duration = 0
-                        twist = command.twist
-                        twist.linear_z = 1
-                        base.SendTwistCommand(command)
-                        base.Stop()
-                    if tz <= -100:
-                        command = Base_pb2.TwistCommand()
-                        command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_TOOL
-                        command.duration = 0
-                        twist = command.twist
-                        twist.linear_z = -1
-                        base.SendTwistCommand(command)
-                        base.Stop()
-          
+                    if tx >= 100: CreateCommand(1,"x","linear")
+                    if tx <= -100: CreateCommand(-1,"x","linear")
+                    if ty >= 100: CreateCommand(1,"y","linear")
+                    if ty <= -100: CreateCommand(-1,"y","linear")
+                    if tz >= 100: CreateCommand(1,"z","linear")
+                    if tz <= -100: CreateCommand(-1,"z","linear")
                 if data[0] == 2:
                     rx = data[1] + (data[2]*256)
                     ry = data[3] + (data[4]*256)
                     rz = data[5] + (data[6]*256)
-                    if data[2] > 127:
-                        rx -= 65536
-                    if data[4] > 127:
-                        ry -= 65536
-                    if data[6] > 127:
-                        rz -= 65536
+                    if data[2] > 127:rx -= 65536
+                    if data[4] > 127:ry -= 65536
+                    if data[6] > 127:rz -= 65536
                     print ("R: ", rx, ry, rz)
-                    if rx >= 100:
-                        command = Base_pb2.TwistCommand()
-                        command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_TOOL
-                        command.duration = 0
-                        twist = command.twist
-                        twist.angular_x = 70
-                        base.SendTwistCommand(command)
-                        base.Stop()
-                    if rx <= -100:
-                        command = Base_pb2.TwistCommand()
-                        command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_TOOL
-                        command.duration = 0
-                        twist = command.twist
-                        twist.angular_x = -70
-                        base.SendTwistCommand(command)
-                        base.Stop() 
-                    if ry >= 100:
-                        command = Base_pb2.TwistCommand()
-                        command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_TOOL
-                        command.duration = 0
-                        twist = command.twist
-                        twist.angular_y = 70
-                        base.SendTwistCommand(command)
-                        base.Stop()
-                    if ry <= -100:
-                        command = Base_pb2.TwistCommand()
-                        command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_TOOL
-                        command.duration = 0
-                        twist = command.twist
-                        twist.angular_y = -70
-                        base.SendTwistCommand(command)
-                        base.Stop()
-                    if rz >= 100:
-                        command = Base_pb2.TwistCommand()
-                        command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_TOOL
-                        command.duration = 0
-                        twist = command.twist
-                        twist.angular_z = 100
-                        base.SendTwistCommand(command)
-                        base.Stop()
-                    if rz <=  -100:
-                        command = Base_pb2.TwistCommand()
-                        command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_TOOL
-                        command.duration = 0
-                        twist = command.twist
-                        twist.angular_z = -100
-                        base.SendTwistCommand(command)
-                        base.Stop()
+                    if rx >= 100: CreateCommand(70,"x","angular")
+                    if rx <= -100: CreateCommand(-70,"x","angular") 
+                    if ry >= 100: CreateCommand(70,"y","angular")
+                    if ry <= -100: CreateCommand(-70,"y","angular")
+                    if rz >= 100: CreateCommand(100,"z","angular")
+                    if rz <=  -100: CreateCommand(-100,"z","angular")
 
                 if data[0] == 3 and data[1] == 0:
                     # Make sure the arm is in Single Level Servoing mode
@@ -185,21 +102,15 @@ def main():
                     for action in action_list.action_list:
                         if action.name == "Home":
                             action_handle = action.handle
-
-                    if action_handle == None:
-                        print("Can't reach safe position.")
-
+                    
                     e = threading.Event()
                     notification_handle = base.OnNotificationActionTopic(
                         check_for_end_or_abort(e),
-                        Base_pb2.NotificationOptions()
-                    )
+                        Base_pb2.NotificationOptions())
                     base.ExecuteActionFromReference(action_handle)
-                        
+                 
             except usb.core.USBError:
-                print("USB error")
-        return 0 if success else 1
-            
+                print("USB error")            
 
 if __name__ == "__main__":
     main()
